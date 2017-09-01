ac4e8ab30cac76a53299d8f2cbbe687681b8e9c3
krish-84 <krishiyer84@gmail.com>
2017-09-01 17:00:50 +0100

    firstcommit

---
+++ CopyofMercury/src/test/java/commonLibsContracts/IChecckBox.java
@@ -0,0 +1,10 @@
+package commonLibsContracts;
+
+import org.openqa.selenium.WebElement;
+
+public interface IChecckBox {
+
+	
+	public void changeCheckBoxStatus(WebElement element, boolean status) throws Exception;
+
+}
